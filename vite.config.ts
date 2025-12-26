25301f2403423a86037aa5c2880dc35309dc8cc8
Ananya Sarkar <ananyamumroy3001@gmail.com>
2025-12-27 00:48:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: "/Portfolio/",  // <-- ADD THIS LINE
+  const env = loadEnv(mode, '.', '');
 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  return {
+    base: "/Portfolio/",
+
+    plugins: [react()],
+
+    css: {
+      postcss: './postcss.config.js',
+    },
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-
-      plugins: [react()],
-
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
-
